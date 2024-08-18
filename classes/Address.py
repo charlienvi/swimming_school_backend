df37252707a513aa347c287eae143b860499285b
Charlien van Impe <63394175+charlienvi@users.noreply.github.com>
2024-08-18 12:55:27 +0200

    test fire structure

---
+++ classes/Address.py
@@ -0,0 +1,8 @@
+class Address:
+    def __init__(self, street, housenumber, zipcode, city):
+        self.street = street
+        self.housenumber = housenumber
+        self.zipcode = zipcode
+        self.city = city
+
+    
